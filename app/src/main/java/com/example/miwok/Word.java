2036eea142de1f54a34c8ea70af2a0863708ba3a
Hemant Gupta <18bcs019@ietdavv.edu.in>
2020-11-20 01:14:34 +0530

    initial commit

---
+++ app/src/main/java/com/example/miwok/Word.java
@@ -0,0 +1,72 @@
+package com.example.miwok;
+
+public class Word {
+
+
+    private int mAudioResourceId;
+
+    private static final int NO_IMAGE_PROVIDED=-1;
+    /** Default translation for the word */
+    private String mDefaultTranslation;
+
+    /** Miwok translation for the word */
+    private String mMiwokTranslation;
+
+    private int mImageResourceId=NO_IMAGE_PROVIDED;
+
+
+    /**
+     * Create a new Word object.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation is the word in the Miwok language
+     */
+    public Word(String defaultTranslation, String miwokTranslation,int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mAudioResourceId = audioResourceId;
+    }
+
+    //new constructor
+
+    public Word(String defaultTranslation, String miwokTranslation,int ImageResourceId,int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId=ImageResourceId;
+        mAudioResourceId = audioResourceId;
+
+    }
+
+    /**
+     * Get the default translation of the word.
+     */
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    /**
+     * Get the Miwok translation of the word.
+     */
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+    public int getImageResourceId()
+    {
+        return mImageResourceId;
+    }
+
+    public boolean hasImage()
+    {
+        return mImageResourceId!=NO_IMAGE_PROVIDED;
+    }
+
+    /**
+     * Return the audio resource ID of the word.
+     */
+    public int getAudioResourceId() {
+        return mAudioResourceId;
+    }
+
+}
